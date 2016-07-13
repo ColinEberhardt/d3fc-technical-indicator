8ccd71154f69659ef49ee2e2c78e3c1056b16ab7
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-07-13 08:17:34 +0100

    feat: changed global name from fc_technical_indicator to simply fc
    BREAKING CHANGE: references to the global variable fc_technical_indicator should be replaced with a reference to fc

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 
 export default {
     entry: 'index.js',
-    moduleName: 'fc_technical_indicator',
+    moduleName: 'fc',
     format: 'umd',
     plugins: [ babel() ],
     dest: 'build/d3fc-technical-indicator.js',
