23e56f8118a8abf34e6179909be741f58bcabc78
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-07-13 11:42:47 +0100

    chore: removed bundle step from npm test

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,12 @@ export default {
     entry: 'index.js',
     moduleName: 'fc',
     format: 'umd',
-    plugins: [ babel() ],
+    plugins: [
+        babel({
+            babelrc: false,
+            presets: ['es2015-rollup']
+        })
+    ],
     dest: 'build/d3fc-technical-indicator.js',
     globals: {
         'd3fc-rebind': 'fc_rebind',
